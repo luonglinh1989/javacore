116ef848dbcc8ae7a291cdd099f57b123430a5f9
luonglinh1989 <101192893+luonglinh1989@users.noreply.github.com>
2022-03-24 22:34:22 +0700

    nopbai

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double a = 6;
+        double b = 3;
+        double c = a/b;
+        double c1 = a+b;
+        double c2 = a-b;
+        double c3 = a*b;
+        double c4 = b/a;
+        System.out.println(c);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+
+        int a1 = 6;
+        int a2 =(int)a1++;
+        System.out.println(a2);
+
+        int a3 = (int)++a1;
+        System.out.println(a3);
+
+
+
+
+    }
+}
